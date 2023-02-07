738e4ed77a3dab911d099ec04385ff7b6ede1795
Sucharita <beherasucharita@gmail.com>
2023-02-07 22:43:31 +0530

    This is my First Program

---
+++ GuestList.py
@@ -0,0 +1,8 @@
+guests = ["Suchi","Mika","Som"]
+
+name = guests[0].title()
+print(name + ", Cordially Inviting you for dinner.")
+name = guests[1].title()
+print(name + ", Would you be joining us for dinner.")
+name = guests[2].title()
+print(name + ", Hey Inviting you for dinner.")
